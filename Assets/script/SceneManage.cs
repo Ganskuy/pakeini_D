ab9ec2cb2e6e3f0f29126f7d17f2dcd01c95651a
Ganskuy <151779150+Ganskuy@users.noreply.github.com>
2024-10-14 12:24:23 +0700

    aye

--- Assets/script/SceneManage.cs
+++ Assets/script/SceneManage.cs
@@ -5,10 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneManage : MonoBehaviour
 {
+    // Method to load Scene 2 (named "lari" in your project)
     public void toScene2(){
         SceneManager.LoadScene("lari");
     }
+
+    // Method to load the Main Menu scene (named "mainmenu" in your project)
     public void toMainMenu(){
         SceneManager.LoadScene("mainmenu");
+    }
+
+    // Method to quit the game
+    public void QuitGame(){
+        // Log a message when the quit function is called
+        Debug.Log("QuitGame called - the application will quit.");
+
+        #if UNITY_EDITOR
+            // Log that we're stopping play mode in the editor
+            Debug.Log("Stopping play mode in the Unity editor.");
+            // Stop play mode in the Unity editor
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // Log that we're quitting the application in a build
+            Debug.Log("Quitting the application.");
+            // Quit the application
+            Application.Quit();
+        #endif
     }
 }
